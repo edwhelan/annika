7b7f71f0dee3b4a8c515c530b4f249fa4e0e34bd
Edward Whelan <vroxing@gmail.com>
2019-01-18 15:38:57 -0500

    centers image on home

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,6 +4,8 @@ import { Link } from 'gatsby'
 import Layout from '../components/layout'
 import SEO from '../components/seo'
 
+import dragon from '../images/dragonPuppetShow.jpg'
+
 // Opening page
 // ----- with picture of us doing dragon puppet show
 // --- icon links to fb and youtube?
@@ -11,7 +13,9 @@ import SEO from '../components/seo'
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" keywords={[`Annika`, `Leisure`, `Puppetry`]} />
-    <p>Welcome to annikas page.</p>
+      <div className='home'>
+        <img src={dragon} alt='shadow puppetry show' />
+      </div>
 
   </Layout>
 )
