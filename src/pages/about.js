a9cdabf82c1d22cec5e529f534b77bf31589dc38
Edward Whelan <vroxing@gmail.com>
2019-01-21 14:55:05 -0500

    adds about page with styles

--- src/pages/about.js
+++ src/pages/about.js
@@ -4,6 +4,8 @@ import { Link } from 'gatsby'
 import Layout from '../components/layout'
 import SEO from '../components/seo'
 
+import muppetfox from '../images/annikaWithPuppet.jpg'
+
 const aboutPage = () => (
   <Layout>
     <SEO title="about me" />
@@ -11,10 +13,10 @@ const aboutPage = () => (
 ---- photo of me and muppet fox
 ----Contact via facebook
 " Annika Leisure is a puppeteer based in Atlanta, Georgia. For more information and up to date news, please visit me on favebook" */}
-
-
-    <p>Welcome to page 2</p>
-    <Link to="/">Go back to the homepage</Link>
+    <div className='about-page-wrapper'>
+      <p>Annika Leisure is a puppeteer based in Atlanta, Georgia. For more information and up to date news, please visit me on facebook</p>
+      <img src={muppetfox} alt='annika with muppet fox' />
+    </div>
   </Layout>
 )
 
